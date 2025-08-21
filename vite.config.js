11d319d599799c4cd758250535b3f207962b4680
yonajin <fionagene@gmail.com>
2025-08-21 19:26:05 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/my-portfolio/",
+  base: "/my-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, ".src/"),
