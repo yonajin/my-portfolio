708e9760a5a5e1171b7f455b9f92d4b4d78605f8
yonajin <fionagene@gmail.com>
2025-08-21 17:12:24 +0800

    deploy setup

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/my-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, ".src/"),
